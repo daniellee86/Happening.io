3beee3f80c0264b9d0458aefd4fc3167d174e203
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-10-09 13:17:33 +0100

    add invite modal and social share buttons

--- pages/event-card.jsx
+++ pages/event-card.jsx
@@ -1,11 +1,90 @@
-import React from "react";
+import { useState } from "react";
+import Modal from "react-modal";
+//modularise modal component
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  EmailShareButton,
+  EmailIcon,
+} from "next-share";
+
+//STYLES FOR MODEL CONVERT TO TAILWIND?
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "50px 100px",
+    border: "solid 1px purple",
+  },
+};
 
 const Event = ({ events, activeEvent }) => {
+  const [showSocial, setShowSocial] = useState(false);
+
+  function openModal() {
+    setShowSocial(true);
+  }
+
+  function closeModal() {
+    setShowSocial(false);
+  }
+
   return (
     <main
       id="event-container"
       className="grid grid-cols-12 py-10 px-5 bg-secondaryThree"
     >
+      <Modal
+        isOpen={showSocial}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Event Card"
+      >
+        <div id="modal-top" className="mb-10">
+          <h1 className="small-title">
+            Invite you friends to {events[activeEvent].Event}
+          </h1>
+        </div>
+        <div id="modal-middle" className="mb-10 flex justify-between">
+          <FacebookShareButton
+            url={"http://localhost:3000/event-card"}
+            quote={"You have been ENVITED!"}
+            hashtag={"#my event by Envited"}
+          >
+            <FacebookIcon size={50} round />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={"http://localhost:3000/event-card"}
+            title={"You have been ENVITED!"}
+          >
+            <TwitterIcon size={50} round />
+          </TwitterShareButton>
+
+          <EmailShareButton
+            url={"http://localhost:3000/event-card"}
+            subject={"Envited event"}
+            body="body"
+          >
+            <EmailIcon size={50} round />
+          </EmailShareButton>
+        </div>
+        <div id="modal-bottom" className="w-full flex justify-center">
+          <button
+            onClick={closeModal}
+            className="w-1/3 bg-purple-500 py-3 px-10 rounded-xl text-white font-bold"
+          >
+            close
+          </button>
+        </div>
+      </Modal>
+
       <section
         id="left"
         className="col-span-6 flex flex-col justify-between items-end"
@@ -23,7 +102,10 @@ const Event = ({ events, activeEvent }) => {
               Start 24h respond timer
             </button>
             <div id="double-button" className="w-full flex justify-between">
-              <button className="w-[45%] p-3 bg-primaryTwo text-white font-bold rounded-xl">
+              <button
+                onClick={() => openModal()}
+                className="w-[45%] p-3 bg-primaryTwo text-white font-bold rounded-xl"
+              >
                 Invite
               </button>
               <button className="w-[45%] p-3  bg-white text-primaryTwo border border-primaryTwo font-bold rounded-xl">
